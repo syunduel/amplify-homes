3606068a52881c482d9152d4dd837210a938a066
shunichiro <5934746+syunduel@users.noreply.github.com>
2023-02-04 11:14:55 +0900

    add Organizations

--- src/routes/collectionList.js
+++ src/routes/collectionList.js
@@ -21,6 +21,20 @@ export default function CollectionList() {
     targetEvmNFTs.push(collectionData["ethereum_0xc067d3e859cbc2c4a8cf9be96bebfa24b0cba5a6"]);
     // Dreamin' Divers Project -Cheers!-
     targetEvmNFTs.push(collectionData["ethereum_0x11c3e2c4329df91f65a16612de90077498cfa6ca"]);
+    // Neo Tokyo Punks
+    targetEvmNFTs.push(collectionData["ethereum_0xa65ba71d653f62c64d97099b58d25a955eb374a0"]);
+    // NFT OCS Collection #1 Drifting Home
+    targetEvmNFTs.push(collectionData["ethereum_0x9f6d1234f654c5bd691ba0b056be80fe3fe3a240"]);
+    // ASAGI
+    targetEvmNFTs.push(collectionData["ethereum_0xdd510ce28dfd085d9cf58f5402ca6d63985e83c0"]);
+    // Flower Lolita Collections
+    targetEvmNFTs.push(collectionData["ethereum_0x81d8e220a6240d1b6dc42d13ed7e0316aa89f265"]);
+    // Neo Samurai Monkeys
+    targetEvmNFTs.push(collectionData["ethereum_0x7fa162d3d44fe8d30e344cc000493da7cb6c6fbb"]);
+    // MetaIdol
+    targetEvmNFTs.push(collectionData["ethereum_0xc059477e8b3f93c86de0e8e05cbd020020ea9a52"]);
+    // Bride Girls Collection
+    targetEvmNFTs.push(collectionData["ethereum_0x2e98069b38c4d8e5c5d995f3fb78d0407fb8b154"]);
     // MediaDAO Friends Nagoya
     targetEvmNFTs.push(collectionData["ethereum_0xa63116b507ab035f8db0b56c7c1405208dd00df4"]);
 
@@ -37,27 +51,46 @@ export default function CollectionList() {
             </h2>
 
             <ul className="organization">
-                <li>
-                    <Link to={`/organization/SoudanNFT/`} style={{textDecoration: 'none'}}>
-                        <div>
-                            SoudanNFT
-                        </div>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`/organization/ODENDAO/`} style={{textDecoration: 'none'}}>
-                        <div>
-                            ODENDAO
-                        </div>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`/organization/Tokyo-Alternative-Girls/`} style={{textDecoration: 'none'}}>
-                        <div>
-                            Tokyo Alternative Girls
-                        </div>
-                    </Link>
-                </li>
+                <li><Link to={`/organization/SoudanNFT/`} style={{textDecoration: 'none'}}>
+                    <div>SoudanNFT</div>
+                </Link></li>
+                <li><Link to={`/organization/ODENDAO/`} style={{textDecoration: 'none'}}>
+                    <div>ODENDAO</div>
+                </Link></li>
+                <li><Link to={`/organization/Tokyo-Alternative-Girls/`} style={{textDecoration: 'none'}}>
+                    <div>Tokyo Alternative Girls</div>
+                </Link></li>
+                <li><Link to={`/organization/Character-DAO/`} style={{textDecoration: 'none'}}>
+                    <div>Character DAO</div>
+                </Link></li>
+                <li><Link to={`/organization/Neo-Tokyo-Punks/`} style={{textDecoration: 'none'}}>
+                    <div>Neo Tokyo Punks</div>
+                </Link></li>
+            </ul>
+            <ul className="organization">
+                <li><Link to={`/organization/Otaku-Culture-Studio/`} style={{textDecoration: 'none'}}>
+                    <div>Otaku Culture Studio</div>
+                </Link></li>
+                <li><Link to={`/organization/Bride-Girls-Collection/`} style={{textDecoration: 'none'}}>
+                    <div>Bride Girls Collection</div>
+                </Link></li>
+                <li><Link to={`/organization/ASAGI/`} style={{textDecoration: 'none'}}>
+                    <div>ASAGI</div>
+                </Link></li>
+                <li><Link to={`/organization/N3-Labs/`} style={{textDecoration: 'none'}}>
+                    <div>N3 Labs</div>
+                </Link></li>
+            </ul>
+            <ul className="organization">
+                <li><Link to={`/organization/MONKEYS/`} style={{textDecoration: 'none'}}>
+                    <div>MONKEYS</div>
+                </Link></li>
+                <li><Link to={`/organization/MetaIdol/`} style={{textDecoration: 'none'}}>
+                    <div>MetaIdol</div>
+                </Link></li>
+                <li><Link to={`/organization/Bride-Girls-Collection/`} style={{textDecoration: 'none'}}>
+                    <div>Bride Girls Collection</div>
+                </Link></li>
             </ul>
 
             <h2 className="title title--secondary">
