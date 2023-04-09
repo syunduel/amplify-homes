a6407a552d7b21e620a5c15d86f5ad743bad6894
shunichiro <5934746+syunduel@users.noreply.github.com>
2023-04-09 23:24:56 +0900

    Alchemy APIが使えるようになった

--- src/App.js
+++ src/App.js
@@ -6,8 +6,42 @@ import CollectionList from './routes/collectionList';
 import Organization from './routes/organization';
 import Collection from './routes/collection';
 import Dressup from "./routes/dressup";
+import { AlchemyMultichainClient } from './lib/alchemy-multichain-client';
+import { Network } from 'alchemy-sdk';
 
 function App() {
+
+  const alchemyTest = async () => {
+
+    // Default config to use for all networks.
+    const defaultConfig = {
+      apiKey: 'upbO14aoCDMzJiLouJJbWfisrDJVunnO', // TODO: Replace with your API key.
+      network: Network.ETH_MAINNET
+    };
+    // Include optional setting overrides for specific networks.
+    const overrides = {
+      // TODO: Replace with your API keys.
+      [Network.MATIC_MAINNET]: { apiKey: 'Izx3-0WVznJi1Rxp4wTcWF6fGIDJs8GI', maxRetries: 10 },
+      [Network.ARB_MAINNET]: { apiKey: 'arb-api-key' }
+    };
+    const alchemy = new AlchemyMultichainClient(defaultConfig, overrides);
+
+    // get NFTs in multiple networks
+    const owner = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045';
+    const mainnetNfts = await alchemy
+      .forNetwork(Network.ETH_MAINNET)
+      .nft.getNftsForOwner(owner, { pageSize: 5 });
+    const maticNfts = await alchemy
+      .forNetwork(Network.MATIC_MAINNET)
+      .nft.getNftsForOwner(owner, { pageSize: 5 });
+
+    console.log('mainnetNfts', mainnetNfts);
+    console.log('maticNfts', maticNfts);
+
+    return <div></div>;
+  }
+
+  alchemyTest();
 
 
   // TODO
